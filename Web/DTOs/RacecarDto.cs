8dbf04bfa171bfd973606107043613f85f7f2eec
Bag Chaser <keondre.m.lucas@my.ccsu.edu>
2021-10-12 12:19:32 -0500

    Racecar Test

--- Web/DTOs/RacecarDto.cs
+++ Web/DTOs/RacecarDto.cs
@@ -7,14 +7,17 @@ namespace Web
         [Required]
         public string Nickname {get; set;}
         [Required]
+        [EnumDataType(typeof(CarModel))] 
         public  CarModel Model {get; set;}
         [Required]
         public string Status {get; set;}
 
         [Required]
+        [Range(300,600)]
         public int TopSpeed {get; set;}
 
         [Required]
+        [EnumDataType(typeof(CarType))] 
         public CarType Type {get; set;}
     }
 
--- test/DTOTests/RacecarDtoTest.cs
+++ test/DTOTests/RacecarDtoTest.cs
@@ -19,13 +19,51 @@ namespace test
         }
 
          [Fact]
-         public void ShouldNotCreateRacecarFromDtoNickName()
+         public void ShouldNotCreateRacecarFromDtoNickname()
         {   
 
             RacecarDto dto = new RacecarDto(){ Nickname = "", Model = CarModel.Nissan, Status = "Available", TopSpeed = 400, Type = CarType.compact};
             var context = new ValidationContext(dto);
             Action act = () => Validator.ValidateObject(dto, context, true);
             act.Should().Throw<ValidationException>().Where(e => e.Message.Contains("required"));
+        }
+
+         [Fact]
+         public void ShouldNotCreateRacecarFromDtoModel()
+        {   
+
+            RacecarDto dto = new RacecarDto(){ Nickname = "Lightning", Model = (CarModel)9, Status = "Available", TopSpeed = 400, Type = CarType.compact};
+            var context = new ValidationContext(dto);
+            Action act = () => Validator.ValidateObject(dto, context, true);
+            act.Should().Throw<ValidationException>().Where(e => e.Message.Contains("invalid"));
+        }
+        [Fact]
+         public void ShouldNotCreateRacecarFromDtoStatus()
+        {   
+
+            RacecarDto dto = new RacecarDto(){ Nickname = "Lightning", Model = (CarModel)5, Status = "", TopSpeed = 400, Type = CarType.compact};
+            var context = new ValidationContext(dto);
+            Action act = () => Validator.ValidateObject(dto, context, true);
+            act.Should().Throw<ValidationException>().Where(e => e.Message.Contains("required"));
+        }
+        [Fact]
+         public void ShouldNotCreateRacecarFromTopSpeed()
+        {   
+
+            RacecarDto dto = new RacecarDto(){ Nickname = "Lightning", Model = (CarModel)5, Status = "Available", TopSpeed = 400, Type = CarType.compact};
+            var context = new ValidationContext(dto);
+            Action act = () => Validator.ValidateObject(dto, context, true);
+            act.Should().Throw<ValidationException>().Where(e => e.Message.Contains("TopSpeed"));
+        }
+
+        [Fact]
+         public void ShouldNotCreateRacecarFromModel()
+        {   
+
+            RacecarDto dto = new RacecarDto(){ Nickname = "Lightning", Model = (CarModel)5, Status = "Available", TopSpeed = 400, Type = (CarType)8};
+            var context = new ValidationContext(dto);
+            Action act = () => Validator.ValidateObject(dto, context, true);
+            act.Should().Throw<ValidationException>().Where(e => e.Message.Contains("invalid"));
         }
     }
 }
